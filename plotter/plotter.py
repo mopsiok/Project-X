6a9ab229b31b9d744477563028474aa5e64d910a
Patryk Jarosz <mopsiok@gmail.com>
2022-07-24 13:48:36 +0200

    data read and preparation

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -1,12 +1,30 @@
 from generic_plot import GenericPlot
+from data_storage import DataStorage
+import message
+
 from enum import Enum
+from pathlib import Path
+import os
+
+#####################################################################
+#                           Config
+#####################################################################
+
+# path to storage directory, where all ProjectX data and backups are stored
+# pointing from this file's directory
+STORAGE_DIRECTORY_PATH = "../server/storage"
+
+
+#####################################################################
+#                           Not config
+#####################################################################
 
 SECONDS_IN_DAY = 60*60*24
 
 
-class PlotterExceptions(Enum):
-    WRONG_ARGS = "Wrong arguments"
-
+#####################################################################
+#                   Project X plotting class
+#####################################################################
 
 class Plotter():
     def __init__(self,  start_date: int = None, 
@@ -28,7 +46,53 @@ class Plotter():
         self.start_date = start_date
         self.end_date = end_date
 
+        print(f"start: {self.start_date}\nend: {self.end_date}\nperiod: {self.period/SECONDS_IN_DAY}")
+
+        dir_path = _get_directory_path() / STORAGE_DIRECTORY_PATH
+        storage = DataStorage(dir_path)
+        data = storage.read_data()
+        data = message.parse_messages(data)
+        self.messages_list = data
+        print(f"Storage read successful. Stored messages count: {len(data)}")
+        if len(data) > 0:
+            print(f"Last message: {message.format(data[-1])}\n")
+
 
     def plot_data(self):
         plot = GenericPlot()
-        print(f"start: {self.start_date}\nend: {self.end_date}\nperiod: {self.period/SECONDS_IN_DAY}")
+        time_list, temp_list, hum_list = self._reshape_data(self.messages_list)
+
+        print("timestamp:")
+        print(" ".join(["%d" % item for item in time_list]))
+        print("\n\ntemperature:")
+        print(" ".join(["%.1f" % item for item in temp_list]))
+        print("\n\nhumidity:")
+        print(" ".join(["%.1f" % item for item in hum_list]))
+
+
+    # Convert list of messages into separate arrays (axes)
+    # Returns a tuple of type: 
+    #   (timestamps: list[int], 
+    #    temperatures: list[float], 
+    #    humidity: list[float])
+    def _reshape_data(self, messages: list):
+        timestamps, temperatures, humidities = [], [], []
+        for msg in messages:
+            t, temp, hum = msg
+            timestamps.append(t)
+            temperatures.append(temp)
+            humidities.append(hum)
+        return timestamps, temperatures, humidities
+
+
+class PlotterExceptions(Enum):
+    WRONG_ARGS = "Wrong arguments"
+
+
+#####################################################################
+#                       Auxiliary functions
+#####################################################################
+
+# returns directory path of given file (this file used if no arguments)
+def _get_directory_path(file : str = __file__):
+    return Path(os.path.dirname(os.path.realpath(file)))
